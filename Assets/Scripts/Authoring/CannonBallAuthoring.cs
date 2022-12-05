ecdbfed4165a29ac0d5e68b7d03bb6949df93e64
Zero2oo <zero2oo.studio@gmail.com>
2022-12-05 11:28:47 +0700

    test change author

--- Assets/Scripts/Authoring/CannonBallAuthoring.cs
+++ Assets/Scripts/Authoring/CannonBallAuthoring.cs
@@ -15,4 +15,9 @@ class CannonBallBaker : Baker<CannonBallAuthoring>
         // So in this case, the Speed field in CannonBall will be float3.zero.
         AddComponent<CannonBall>();
     }
+
+    public void ChangeAuthor()
+    {
+        
+    }
 }
